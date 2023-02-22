7102a7d70736bcc7f51e269bcf640d1d85b92e9a
Vikrom Narula <vikrom.nar@eruditetech.net>
2023-02-22 10:35:11 +0700

    Add back `which-key` back as a library

--- init.lua
+++ init.lua
@@ -95,6 +95,9 @@ require('lazy').setup({
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
