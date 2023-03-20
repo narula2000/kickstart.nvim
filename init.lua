bd42293da56dd2c35e0d7bffb8d7710fe0a1a656
Vikrom Narula <vikrom.nar@eruditetech.net>
2023-03-20 17:39:30 +0700

    Add file explore, variable highlighter, edit config

--- init.lua
+++ init.lua
@@ -236,6 +236,72 @@ require('lazy').setup({
   },
 
   {
+    -- File explorer
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+
+  {
+    -- Variable highlighter
+    'RRethy/vim-illuminate',
+    version = '*',
+    config = function()
+      require("illuminate").configure({
+        -- providers: provider used to get references in the buffer, ordered by priority
+        providers = {
+          'lsp',
+          'treesitter',
+          'regex',
+        },
+        -- delay: delay in milliseconds
+        delay = 100,
+        -- filetype_overrides: filetype specific overrides.
+        -- The keys are strings to represent the filetype while the values are tables that
+        -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+        filetype_overrides = {},
+        -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+        filetypes_denylist = {
+          'dirvish',
+          'fugitive',
+        },
+        -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+        filetypes_allowlist = {},
+        -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+        -- See `:help mode()` for possible values
+        modes_denylist = {},
+        -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+        -- See `:help mode()` for possible values
+        modes_allowlist = {},
+        -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+        -- Only applies to the 'regex' provider
+        -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+        providers_regex_syntax_denylist = {},
+        -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+        -- Only applies to the 'regex' provider
+        -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+        providers_regex_syntax_allowlist = {},
+        -- under_cursor: whether or not to illuminate under the cursor
+        under_cursor = true,
+        -- large_file_cutoff: number of lines at which to use large_file_config
+        -- The `under_cursor` option is disabled when this cutoff is hit
+        large_file_cutoff = nil,
+        -- large_file_config: config to use for large files (based on large_file_cutoff).
+        -- Supports the same keys passed to .configure
+        -- If nil, vim-illuminate will be disabled for large files.
+        large_file_overrides = nil,
+        -- min_count_to_highlight: minimum number of matches required to perform highlighting
+        min_count_to_highlight = 1,
+      })
+    end,
+  },
+
+  {
     -- Syntax highlight comment
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -247,8 +313,8 @@ require('lazy').setup({
     "shortcuts/no-neck-pain.nvim",
     version = "*",
     opts = {
-      autocmds = { enableOnVimEnter = true, },
-      mappings = { toggle = "<leader>np", },
+      autocmds = { enableOnVimEnter = false, },
+      mappings = { enabled = true, toggle = "<leader>np", },
       width = 135,
     },
   },
@@ -362,6 +428,7 @@ vim.o.listchars = "tab:»·,eol:↲,trail:·,extends:»,precedes:«,nbsp:·"
 vim.o.showbreak = "↪ "
 
 vim.keymap.set('n', 'Y', 'y$')
+vim.keymap.set('n', '<leader>fe', '<cmd>NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>+', '<cmd>vertical resize +5<CR>')
 vim.keymap.set('n', '<leader>-', '<cmd>vertical resize -5<CR>')
 vim.keymap.set('n', '<leader>S', '<cmd>set spell!<CR>')
